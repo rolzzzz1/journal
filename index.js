5927261e81bc4b92672f4651b0b934ab93a9b6e1
Aishwarya Sharma <roli.mradul@gmail.com>
2024-05-17 15:15:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ heroDiv.addEventListener("click", function () {
                                         </header>
                                     </article>
                                     <article>
-                                        <img src="/.netlify/images?url=/images/article-image-01.jpg" 
+                                        <img src="article-image-hero-Cpw6ye6q.jpg" 
                                             alt="A laptop on desk with visible coding script" class="articleImg">
                                         <header class="article-header">
                                             <p>July 23, 2022</p>
